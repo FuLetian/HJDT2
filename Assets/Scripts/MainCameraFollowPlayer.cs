083351cefd789dee4bbbf197e1f23ca31b0c1018
FuLetian <letian.fu@monstar-lab.com>
2015-03-18 10:28:12 +0800

    jhguyg

--- Assets/Scripts/MainCameraFollowPlayer.cs
+++ Assets/Scripts/MainCameraFollowPlayer.cs
@@ -4,8 +4,13 @@ using System.Collections;
 public class MainCameraFollowPlayer : MonoBehaviour {
 
 	public GameObject player;
+	public GameObject positionFlag1;
 
 	public bool isConsumerActive;
+
+	public float smoothing;
+
+	public bool catchPostionFlag1;
 
 	// Use this for initialization
 	void Start () {
@@ -18,8 +23,21 @@ public class MainCameraFollowPlayer : MonoBehaviour {
 		//this.transform.position = player.transform.position + fromPosition;
 		if(isConsumerActive){
 			Vector3 p = transform.position;
-			p.x = player.transform.position.x;
-			transform.position = p;
+			if(player.transform.position.x > transform.position.x){
+				p.x = player.transform.position.x;
+				transform.position = p;
+			}
+
+			if(catchPostionFlag1 == false && transform.position.x-positionFlag1.transform.position.x > 0){
+
+				Vector3 selfP = transform.position;
+				selfP.y += smoothing *Time.deltaTime;
+				transform.position = selfP;
+
+				if(transform.position.y - positionFlag1.transform.position.y > 0){
+					catchPostionFlag1 = true;
+				}
+			}
 		}
 	}
 }
